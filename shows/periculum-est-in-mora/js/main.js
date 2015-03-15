a35e51acb89d0c4f2b9520800df475637c42d8c8
Alex Karlovich <alexkarlovich@gmail.com>
2015-03-15 06:37:20 +0300

    PEiM

---
+++ shows/periculum-est-in-mora/js/main.js
@@ -0,0 +1,37 @@
+var audioElement = document.getElementById('audioElement');
+var videoElement = document.getElementById('momVideo');
+Reveal.addEventListener( 'fragmentshown', function( event ) {
+	if(event.fragment.id === 'beep-fragment') {
+		audioElement.src="beep.mp3";
+		audioElement.volume = 0.2;
+		audioElement.play();
+	}
+});
+
+// Reveal.addEventListener( 'fragmentshown', function( event ) {
+// 	if(event.fragment.id === 'beep-fragment') {
+// 		audio.src="beep.mp3"
+// 	}
+// });
+
+var videoPlayed = false;
+Reveal.addEventListener( 'fragmentshown', function( event ) {
+	if(event.fragment.id === 'momVideo' && videoPlayed !== true) {
+		videoPlayed = true;
+		//window.videoTimeout = window.setTimeout(function() {
+			console.log('setTimeout handler');
+			videoElement.src = "Mom.mp4";
+			videoElement.play();
+		//}, 300);
+	}
+});
+
+// Reveal.addEventListener( 'fragmenthidden', function( event ) {
+// 	console.log('fragmenthidden' + window.videoTimeout);
+// 	if(event.fragment.id === 'momVideo') {
+// 		if(window.videoTimeout != null) {
+// 			window.clearTimeout(window.videoTimeout);
+// 			window.videoTimeout = null;
+// 		}
+// 	}
+// });
